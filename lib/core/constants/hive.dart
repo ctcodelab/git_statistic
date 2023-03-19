e7a8a1a4b6f34bd281d1b995a92e2d4aff3c54c1
Yevhen <ybeshkarov@icloud.com>
2023-03-19 12:44:26 +0200

    unit tests

--- lib/core/constants/hive.dart
+++ lib/core/constants/hive.dart
@@ -5,3 +5,10 @@ class HiveId {
   static const memberId = 1;
   static const sharedMemberId = 2;
 }
+
+class HiveBox {
+  HiveBox._();
+
+  static const preferencesBox = '_preferences';
+  static const securedBox = '_securedBox';
+}
--- lib/core/services/hive/hive_service_imp.dart
+++ lib/core/services/hive/hive_service_imp.dart
@@ -1,11 +1,13 @@
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:git_statistic/core/services/hive/hive_service.dart';
 import 'package:git_statistic/features/onboarding/domain/entities/member_entity.dart';
 import 'package:git_statistic/features/onboarding/domain/entities/members_entity.dart';
 import 'package:git_statistic/features/share_account/domain/entities/shared_member_entity.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveServiceImpl implements HiveService {
-  late final Box _box;
+  HiveServiceImpl([this._box]);
+
+  late final Box? _box;
 
   static const onboardingPassed = 'onboardingPassed';
   static const trackedProjectId = 'trackedProjectId';
@@ -28,19 +30,19 @@ class HiveServiceImpl implements HiveService {
 
   @override
   T getValue<T>(dynamic key, {T? defaultValue}) {
-    return _box.get(key, defaultValue: defaultValue) as T;
+    return _box!.get(key, defaultValue: defaultValue) as T;
   }
 
   @override
   Future setValue<T>(dynamic key, T value) {
-    return _box.put(key, value);
+    return _box!.put(key, value);
   }
 
   @override
   Future<int> clear() {
-    return _box.clear();
+    return _box!.clear();
   }
 
   @override
-  Stream<BoxEvent> listenKeyChanges(dynamic key) => _box.watch(key: key);
+  Stream<BoxEvent> listenKeyChanges(dynamic key) => _box!.watch(key: key);
 }
